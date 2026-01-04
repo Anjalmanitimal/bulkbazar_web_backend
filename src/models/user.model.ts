8c9c46a255ba2ff08545c2837ae80c70d47a3e56
anjal <anjalmani2023@gmail.com>
2026-01-04 23:49:40 +0545

    models and final folder structure complete

---
+++ src/models/user.model.ts
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+export const UserModel = model("User", userSchema);
