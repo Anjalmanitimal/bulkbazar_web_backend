dcfb834007d189378654fbc2ca226bbef49295f0
anjal <anjalmani2023@gmail.com>
2026-02-21 01:34:34 +0545

    product upload middleware created

---
+++ src/middlewares/productUpload.middleware.ts
@@ -0,0 +1,14 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: "uploads/products",
+
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+export const uploadProductImage = multer({
+  storage,
+});
